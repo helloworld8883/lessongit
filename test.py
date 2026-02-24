65a45161e61ad1dc48a30cd87c40021285319106
helloworl883 <figer756@icloud.com>
2026-02-24 20:21:29 +0100

    my commit

--- test.py
+++ test.py
@@ -2,3 +2,6 @@ print("hi Misha")
 #1 step git add . add changes
 #2 step git commit -m "my commit" -saving changes
 #3 step git push origin main - sending the whole thing .
+
+
+# git clone is only working in the start 
